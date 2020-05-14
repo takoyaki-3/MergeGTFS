9bcb0a3ee9e4c31e79da7ba917fa576b0c6aac82
Hatano Yuuta <hatano.yuuta@gmail.com>
2020-05-14 11:07:02 +0900

    load csv header

--- merge.go
+++ merge.go
@@ -10,11 +10,16 @@ import (
 		"strconv"
 		"strings"
 		"sync"
+		"encoding/csv"
 )
 
 var wg_unzip sync.WaitGroup
+var num_of_gtfs int
 
 func main() {
+	// initi
+	num_of_gtfs = 0
+
 	// unzip all gtfs
 	paths := dirwalk("./GTFS")
 	if err := os.Mkdir("./unzip/", 0777); err != nil {
@@ -24,16 +29,59 @@ func main() {
 		if(!strings.HasSuffix(path, ".zip")){
 			continue
 		}
-		fmt.Println(index)
+//		fmt.Println(index)
 		fmt.Println(path)
 		if err := os.Mkdir("./unzip/"+strconv.Itoa(index), 0777); err != nil {
 			fmt.Println(err)
 		}
 		wg_unzip.Add(1)
 		go Unzip(path,"./unzip/"+strconv.Itoa(index))
+		num_of_gtfs++
 	}
 	wg_unzip.Wait()
+
+	// load csv
+	for i:=0;i<num_of_gtfs;i++ {
+		head,data := load_gtfs_file(i,"agency")
+
+		fmt.Println(head)
+		fmt.Println(data)
+	}
 	fmt.Println("end")
+}
+
+func load_gtfs_file(index int,filename string) (head map[string]int,records [][]string) {
+
+	head = map[string]int{}
+	records = [][]string{}
+
+	file, err := os.Open("./unzip/"+strconv.Itoa(index)+"/"+filename+".txt")
+	if err != nil {
+			panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	isf := true
+	for {
+		var line []string
+		line, err = reader.Read()
+		if err != nil {
+				break
+		}
+		if isf {
+			for ind,str := range line {
+				head[str] = ind
+			}
+			isf=false
+			continue
+		}
+
+		records = append(records,line)
+	}
+
+	return
 }
 
 func dirwalk(dir string) []string {
